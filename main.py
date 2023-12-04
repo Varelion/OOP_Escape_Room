f92669d104561468ab6cc323b018c2c9e64bbd7f
Varelion <business.8508@gmail.com>
2023-12-04 10:09:24 -0500

    Establish Class and init method

---
+++ main.py
@@ -0,0 +1,22 @@
+class GameObject:
+	# Note: fields can be created with only the initializer function.
+	name = ""
+	appearance = ""
+	feel = ""
+	smell = ""
+
+# Every initializer needs to have this syntax, of __ and init and a 'self' parameter.
+	def __init__(self, name, appearance, feel, smell):
+		self.name = name
+		self.appearance = appearance
+		self.feel = feel
+		self.smell = smell
+
+	def look(self):
+		return f"You look the {self.name}. \n {self.appearance}\n"
+
+	def feel(self):
+		return f"You feel the {self.name}. \n {self.feel}\n"
+
+	def sniff(self):
+		return f"You sniff the {self.name}. \n {self.smell}\n"
