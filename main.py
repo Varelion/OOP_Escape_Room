e7071d00187af490e8fe94993e525576d3a0d511
Varelion <business.8508@gmail.com>
2023-12-04 11:06:09 -0500

    Create take_turn method to Game in main.py

--- main.py
+++ main.py
@@ -51,3 +51,38 @@ class Room:
 		for object in objects_in_game:
 			names.append(object.name)
 		return names
+
+class Game:
+	def __init__(self, room):
+		self.attempts = 0
+		Objects = self.create_objects
+		self.room = Room(111,[])
+
+	def create_objects(self):
+        return [
+          GameObject(
+            "Sweater",
+            "It's a blue sweater that had the number 12 switched on it.",
+            "Someone has unstitched the second number, leaving only the 1.",
+            "The sweater smells of laundry detergent."),
+          GameObject(
+            "Chair",
+            "It's a wooden chair with only 3 legs.",
+            "Someone had deliberately snapped off one of the legs.",
+            "It smells like old wood."),
+          GameObject(
+            "Journal",
+            "The final entry states that time should be hours then minutes then seconds (H-M-S).",
+            "The cover is worn and several pages are missing.",
+            "It smells like musty leather."),
+          GameObject(
+            "Bowl of soup",
+            "It appears to be tomato soup.",
+            "It has cooled down to room temperature.",
+            "You detect 7 different herbs and spices."),
+          GameObject(
+            "Clock",
+            "The hour hand is pointing towards the soup, the minute hand towards the chair, and the second hand towards the sweater.",
+            "The battery compartment is open and empty.",
+            "It smells of plastic."),
+        ]
